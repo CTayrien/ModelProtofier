c6ad76ae9e5c26d34cfa0036c6fa689a489f1045
cyprian <cypriantayrien@gmail.com>
2017-04-02 01:06:42 -0400

    Added gitignore, attributes, working project and compiled .zip

---
+++ Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace ModelProcessor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 263);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Import FIle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(138, 263);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Export File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.richTextBox1.Location = new System.Drawing.Point(16, 13);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(230, 132);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "max | x |";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(28, 184);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "max | y |";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(28, 210);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "max | z |";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(26, 237);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "# verts";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(138, 154);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(93, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(138, 180);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(93, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(138, 207);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(93, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(138, 234);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(93, 20);
+            this.textBox4.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(258, 299);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Model Protofier";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+//using System.Collections;
+
+namespace ModelProcessor
+{
+    public partial class Form1 : Form
+    {
+        struct vec2
+        {
+            public float x, y;
+        }
+        struct vec3
+        {
+            public float x, y, z;
+        }
+        struct Vertex {
+            public vec3 loc;
+            public vec2 uv;
+            public vec3 norm;
+        }
+        struct VertInd
+        {
+            public uint locInd, uvInd, normInd;
+        }
+
+        List<Vertex> vertBufData = new List<Vertex>();
+        float maxx = 0, maxy = 0, maxz = 0;
+        uint nverts = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // disable the buttons
+            button1.Enabled = false;
+            button2.Enabled = false;
+
+            // Open file dialog
+            OpenFileDialog open = new OpenFileDialog();
+            open.ShowDialog();
+            try
+            {
+                // assume texture-mapped, normal mapped and triangulated faces
+                // try to import obj file data into buffer
+
+                // PROCESS
+                // Get unique vertex locs, uvs and norms
+                List<vec3> locs = new List<vec3>();
+                List<vec2> uvs = new List<vec2>();
+                List<vec3> norms = new List<vec3>();
+
+                // Get indices of locations, uvs and normals for stitching
+                List<VertInd> vertInds = new List<VertInd>();
+
+                // The following will be parsed from the file...
+                //Vertex vertex;
+                //VertInd vertInd;
+
+                // Get all lines from the file, one at a time.
+                StreamReader modelstream = new StreamReader(open.FileName);
+                string lineString;
+                while ((lineString = modelstream.ReadLine()) != null)
+                {
+                    char[] delims = { ' ', '/' };
+                    string[] words = lineString.Split(delims);
+
+                    // Parse label
+                    string label = words[0];
+
+                    // Parse and add a loc
+                    if (label == "v")
+                    {
+                        vec3 loc = new vec3();
+                        float.TryParse(words[1], out loc.x);
+                        float.TryParse(words[2], out loc.y);
+                        float.TryParse(words[3], out loc.z);
+                        locs.Add(loc);
+                    }
+                    // Parse and add a uv
+                    if (label == "vt")
+                    {
+                        vec2 uv = new vec2();
+                        float.TryParse(words[1], out uv.x);
+                        float.TryParse(words[2], out uv.y);
+                        uvs.Add(uv);
+                    }
+                    // Parse and add a norm
+                    if (label == "vn")
+                    {
+                        vec3 norm = new vec3();
+                        float.TryParse(words[1], out norm.x);
+                        float.TryParse(words[2], out norm.y);
+                        float.TryParse(words[3], out norm.z);
+                        norms.Add(norm);
+                    }
+                    // Parse and add 3 complex vertInds
+                    if (label == "f")
+                    {
+                        for (int i = 0; i < 3; i++)
+                        {
+                            VertInd vertInd = new VertInd();
+                            UInt32.TryParse(words[1 + i * 3], out vertInd.locInd); vertInd.locInd--;
+                            UInt32.TryParse(words[2 + i * 3], out vertInd.uvInd); vertInd.uvInd--;
+                            UInt32.TryParse(words[3 + i * 3], out vertInd.normInd); vertInd.normInd--;
+                            vertInds.Add(vertInd);
+                        }
+                    }
+                }
+                
+                modelstream.Close();
+
+                // Recenter model
+                vec3 mina = new vec3();
+                vec3 maxa = new vec3();
+                vec3 c = new vec3();
+
+                mina.x = maxa.x = locs[0].x;
+                mina.y = maxa.y = locs[0].y;
+                mina.z = maxa.z = locs[0].z;
+
+                for (int i = 1; i < locs.Count; i++)
+                {
+                    if (locs[i].x < mina.x) mina.x = locs[i].x;
+                    if (locs[i].y < mina.y) mina.y = locs[i].y;
+                    if (locs[i].z < mina.z) mina.z = locs[i].z;
+
+                    if (locs[i].x > maxa.x) maxa.x = locs[i].x;
+                    if (locs[i].y > maxa.y) maxa.y = locs[i].y;
+                    if (locs[i].z > maxa.z) maxa.z = locs[i].z;
+                }
+
+                c.x = (mina.x + maxa.x) / 2;
+                c.y = (mina.y + maxa.y) / 2;
+                c.z = (mina.z + maxa.z) / 2;
+
+                List<vec3> locsNew = new List<vec3>();
+                for (int i = 0; i < locs.Count; i++)
+                {
+                    vec3 loc = new vec3();
+                    loc.x = locs[i].x - c.x;
+                    loc.y = locs[i].y - c.y;
+                    loc.z = locs[i].z - c.z;
+                    locsNew.Add(loc);
+                }
+                locs = locsNew;
+
+                // Get max extremeties (needed for collision checks)
+                for (int i = 0; i < locs.Count; i++)
+                {
+                    vec3 abs = new vec3();
+                    abs.x = Math.Abs(locs[i].x);
+                    abs.y = Math.Abs(locs[i].y);
+                    abs.z = Math.Abs(locs[i].z);
+
+                    //if (r > maxr) maxr = r;
+                    if (abs.x > maxx) maxx = abs.x;
+                    if (abs.y > maxy) maxy = abs.y;
+                    if (abs.z > maxz) maxz = abs.z;
+                }
+
+                // Stitch together data for vertex buffer
+                nverts = (uint)vertInds.Count;
+                //vertBufData = new List<Vertex>((int)nverts);
+                for (int i = 0; i < nverts; i++)
+                {
+                    Vertex vertex = new Vertex();
+
+                    vertex.loc = locs[(int)vertInds[i].locInd];
+                    vertex.uv = uvs[(int)vertInds[i].uvInd];
+                    vertex.norm = norms[(int)vertInds[i].normInd];
+
+                    vertBufData.Add(vertex);
+                }
+
+                textBox1.Text = maxx.ToString();
+                textBox2.Text = maxy.ToString();
+                textBox3.Text = maxz.ToString();
+                textBox4.Text = nverts.ToString();
+
+                // popup with success or failure
+                MessageBox.Show("Importing completed");
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message);
+                MessageBox.Show("Error reading file");
+            }
+            
+            // enable the buttons
+            button1.Enabled = true;
+            button2.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // disable the buttons
+            button1.Enabled = false;
+            button2.Enabled = false;
+
+            // Save file dialog
+            SaveFileDialog save = new SaveFileDialog();
+            save.DefaultExt = ".dat";
+            save.ShowDialog();
+            try
+            {
+                //assemble byte array
+                int nbytes = 16 + 32 * (int)nverts;
+                byte[] data = new byte[nbytes];
+                data[0] = (byte)nbytes;
+
+                System.IO.Stream modelstream = save.OpenFile();
+                BinaryWriter bw = new BinaryWriter(modelstream);
+
+                // try to write buffer data into file
+                // write 4 byte unsigned int (long long) number of bytes in buffer
+                bw.Write(maxx);
+                bw.Write(maxy);
+                bw.Write(maxz);
+                bw.Write(nverts);
+
+                for (int i=0; i < nverts; i++)
+                {
+                    bw.Write(vertBufData[i].loc.x);
+                    bw.Write(vertBufData[i].loc.y);
+                    bw.Write(vertBufData[i].loc.z);
+
+                    bw.Write(vertBufData[i].uv.x);
+                    bw.Write(vertBufData[i].uv.y);
+
+                    bw.Write(vertBufData[i].norm.x);
+                    bw.Write(vertBufData[i].norm.y);
+                    bw.Write(vertBufData[i].norm.z);
+                }
+
+                modelstream.Close();
+                
+                // popup with success or failure
+                MessageBox.Show("Exporting completed");
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message);
+                MessageBox.Show("Error writing file");
+            }
+
+            vertBufData = new List<Vertex>();
+            maxx = maxy = maxz = 0;
+            nverts = 0;
+
+            textBox1.Text = maxx.ToString();
+            textBox2.Text = maxy.ToString();
+            textBox3.Text = maxz.ToString();
+            textBox4.Text = nverts.ToString();
+
+            // enable the buttons
+            button1.Enabled = true;
+            button2.Enabled = true;
+        }
+    }
+}
