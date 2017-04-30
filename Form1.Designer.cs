a14910b8e07a36dc77c3ea864dc62972c2a55888
cbtigm <cbtigm@GDD-17>
2017-04-30 09:52:16 -0400

    Added maxr, and some toggles/buttons for recentering and scaling. Reprocessed files in engine/models, called them quad2.dat for example, but have not reprocessed other models. Have not tested any new models or collisions, as there is currently no scene. Going to make a collision/quaternion satellite docking scene and test engine models and find some new models to use in the scene.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -41,21 +41,25 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(16, 263);
+            this.button1.Location = new System.Drawing.Point(156, 202);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Import FIle";
+            this.button1.Text = "Import File";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(138, 263);
+            this.button2.Location = new System.Drawing.Point(169, 376);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
@@ -77,7 +81,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(28, 158);
+            this.label1.Location = new System.Drawing.Point(28, 243);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(61, 13);
             this.label1.TabIndex = 3;
@@ -87,7 +91,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(28, 184);
+            this.label2.Location = new System.Drawing.Point(28, 269);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(61, 13);
             this.label2.TabIndex = 4;
@@ -97,7 +101,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(28, 210);
+            this.label3.Location = new System.Drawing.Point(28, 295);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(61, 13);
             this.label3.TabIndex = 5;
@@ -107,7 +111,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(26, 237);
+            this.label4.Location = new System.Drawing.Point(26, 349);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(49, 13);
             this.label4.TabIndex = 6;
@@ -115,7 +119,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(138, 154);
+            this.textBox1.Location = new System.Drawing.Point(138, 239);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(93, 20);
@@ -123,7 +127,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(138, 180);
+            this.textBox2.Location = new System.Drawing.Point(138, 265);
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
             this.textBox2.Size = new System.Drawing.Size(93, 20);
@@ -131,7 +135,7 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(138, 207);
+            this.textBox3.Location = new System.Drawing.Point(138, 292);
             this.textBox3.Name = "textBox3";
             this.textBox3.ReadOnly = true;
             this.textBox3.Size = new System.Drawing.Size(93, 20);
@@ -139,17 +143,63 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(138, 234);
+            this.textBox4.Location = new System.Drawing.Point(138, 347);
             this.textBox4.Name = "textBox4";
             this.textBox4.ReadOnly = true;
             this.textBox4.Size = new System.Drawing.Size(93, 20);
             this.textBox4.TabIndex = 10;
             // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(138, 320);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(93, 20);
+            this.textBox5.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(28, 321);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "max | r |";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(28, 161);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(110, 17);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Text = "recenter model";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.Location = new System.Drawing.Point(28, 184);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(104, 17);
+            this.checkBox2.TabIndex = 14;
+            this.checkBox2.Text = "rescale model";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(258, 299);
+            this.ClientSize = new System.Drawing.Size(258, 411);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
@@ -184,6 +234,10 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -35,13 +35,98 @@ namespace ModelProcessor
             public uint locInd, uvInd, normInd;
         }
 
+        // Unique vertex locs, uvs and norms
+        List<vec3> locs = new List<vec3>();
+        List<vec2> uvs = new List<vec2>();
+        List<vec3> norms = new List<vec3>();
+
+        // Indices of locations, uvs and normals for stitching
+        List<VertInd> vertInds = new List<VertInd>();
+
+        // Stitched data going into .dat, designed for direct uploading to vram
         List<Vertex> vertBufData = new List<Vertex>();
-        float maxx = 0, maxy = 0, maxz = 0;
+
+        // Model stats prepended into.dat
+
+        // Needed for collision or buffer/draw calls
+        float maxx = 0, maxy = 0, maxz = 0, maxr = 0;
         uint nverts = 0;
 
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void recenter()
+        {
+            vec3 mina = new vec3();
+            vec3 maxa = new vec3();
+            vec3 c = new vec3();
+
+            mina.x = maxa.x = locs[0].x;
+            mina.y = maxa.y = locs[0].y;
+            mina.z = maxa.z = locs[0].z;
+
+            for (int i = 1; i < locs.Count; i++)
+            {
+                if (locs[i].x < mina.x) mina.x = locs[i].x;
+                if (locs[i].y < mina.y) mina.y = locs[i].y;
+                if (locs[i].z < mina.z) mina.z = locs[i].z;
+
+                if (locs[i].x > maxa.x) maxa.x = locs[i].x;
+                if (locs[i].y > maxa.y) maxa.y = locs[i].y;
+                if (locs[i].z > maxa.z) maxa.z = locs[i].z;
+            }
+
+            c.x = (mina.x + maxa.x) / 2;
+            c.y = (mina.y + maxa.y) / 2;
+            c.z = (mina.z + maxa.z) / 2;
+
+            List<vec3> locsNew = new List<vec3>();
+            for (int i = 0; i < locs.Count; i++)
+            {
+                vec3 loc = new vec3();
+                loc.x = locs[i].x - c.x;
+                loc.y = locs[i].y - c.y;
+                loc.z = locs[i].z - c.z;
+                locsNew.Add(loc);
+            }
+            locs = locsNew;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+                if(nverts > 0)
+                    recenter();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+                if (nverts > 0)
+                    rescale();
+        }
+
+        private void rescale()
+        {
+            float maxr = 0;
+            for (int i = 0; i < locs.Count; i++)
+            {
+                float r = (float)Math.Sqrt((locs[i].x * locs[i].x + locs[i].y * locs[i].y + locs[i].z * locs[i].z));
+                if (r > maxr) maxr = r;
+            }
+
+            List<vec3> locsNew = new List<vec3>();
+            for (int i = 0; i < locs.Count; i++)
+            {
+                vec3 loc = new vec3();
+                loc.x = locs[i].x / maxr;
+                loc.y = locs[i].y / maxr;
+                loc.z = locs[i].z / maxr;
+                locsNew.Add(loc);
+            }
+            locs = locsNew;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -55,22 +140,9 @@ namespace ModelProcessor
             open.ShowDialog();
             try
             {
-                // assume texture-mapped, normal mapped and triangulated faces
+                // assume normal mapped and triangulated faces
                 // try to import obj file data into buffer
-
-                // PROCESS
-                // Get unique vertex locs, uvs and norms
-                List<vec3> locs = new List<vec3>();
-                List<vec2> uvs = new List<vec2>();
-                List<vec3> norms = new List<vec3>();
-
-                // Get indices of locations, uvs and normals for stitching
-                List<VertInd> vertInds = new List<VertInd>();
-
-                // The following will be parsed from the file...
-                //Vertex vertex;
-                //VertInd vertInd;
-
+                
                 // Get all lines from the file, one at a time.
                 StreamReader modelstream = new StreamReader(open.FileName);
                 string lineString;
@@ -111,9 +183,6 @@ namespace ModelProcessor
                     // Parse and add 3 complex vertInds
                     if (label == "f")
                     {
-
-                        
-
                         for (int i = 0; i < 3; i++)
                         {
                             int locWord = 1 + i * 3;
@@ -139,76 +208,45 @@ namespace ModelProcessor
                 }
 
                 // Recenter model
-                vec3 mina = new vec3();
-                vec3 maxa = new vec3();
-                vec3 c = new vec3();
-
-                mina.x = maxa.x = locs[0].x;
-                mina.y = maxa.y = locs[0].y;
-                mina.z = maxa.z = locs[0].z;
-
-                for (int i = 1; i < locs.Count; i++)
+                if (checkBox1.Checked)
                 {
-                    if (locs[i].x < mina.x) mina.x = locs[i].x;
-                    if (locs[i].y < mina.y) mina.y = locs[i].y;
-                    if (locs[i].z < mina.z) mina.z = locs[i].z;
-
-                    if (locs[i].x > maxa.x) maxa.x = locs[i].x;
-                    if (locs[i].y > maxa.y) maxa.y = locs[i].y;
-                    if (locs[i].z > maxa.z) maxa.z = locs[i].z;
+                    recenter();
                 }
 
-                c.x = (mina.x + maxa.x) / 2;
-                c.y = (mina.y + maxa.y) / 2;
-                c.z = (mina.z + maxa.z) / 2;
-
-                List<vec3> locsNew = new List<vec3>();
-                for (int i = 0; i < locs.Count; i++)
+                // Rescale model
+                if (checkBox2.Checked)
                 {
-                    vec3 loc = new vec3();
-                    loc.x = locs[i].x - c.x;
-                    loc.y = locs[i].y - c.y;
-                    loc.z = locs[i].z - c.z;
-                    locsNew.Add(loc);
+                    rescale();
                 }
-                locs = locsNew;
 
                 // Get max extremeties (needed for collision checks)
                 for (int i = 0; i < locs.Count; i++)
                 {
+                    float r = (float)Math.Sqrt((locs[i].x * locs[i].x + locs[i].y * locs[i].y + locs[i].z * locs[i].z));
+                    if (r > maxr) maxr = r;
+
                     vec3 abs = new vec3();
                     abs.x = Math.Abs(locs[i].x);
                     abs.y = Math.Abs(locs[i].y);
                     abs.z = Math.Abs(locs[i].z);
-
-                    //if (r > maxr) maxr = r;
+                    
                     if (abs.x > maxx) maxx = abs.x;
                     if (abs.y > maxy) maxy = abs.y;
                     if (abs.z > maxz) maxz = abs.z;
                 }
 
-                // Stitch together data for vertex buffer
                 nverts = (uint)vertInds.Count;
-                //vertBufData = new List<Vertex>((int)nverts);
-                for (int i = 0; i < nverts; i++)
-                {
-                    int iLoc = (int)vertInds[i].locInd;
-                    int iuv =  (int)vertInds[i].uvInd;
-                    int inorm = (int)vertInds[i].normInd;
-                    
-                    Vertex vertex = new Vertex();
-                    vertex.loc = locs[iLoc];
-                    vertex.uv = uvs[iuv];
-                    vertex.norm = norms[inorm];
-                    vertBufData.Add(vertex);
-                }
-
+                
                 textBox1.Text = maxx.ToString();
                 textBox2.Text = maxy.ToString();
                 textBox3.Text = maxz.ToString();
+                textBox5.Text = maxr.ToString();
                 textBox4.Text = nverts.ToString();
 
-                // popup with success or failure
+                checkBox1.Checked = false;
+                checkBox2.Checked = false;
+
+                // pop-up with success or failure
                 MessageBox.Show("Importing completed");
             }
             catch (Exception ex)
@@ -235,6 +273,20 @@ namespace ModelProcessor
             try
             {
                 //assemble byte array
+                // Stitch together data for vertex buffer
+                for (int i = 0; i < nverts; i++)
+                {
+                    int iLoc = (int)vertInds[i].locInd;
+                    int iuv = (int)vertInds[i].uvInd;
+                    int inorm = (int)vertInds[i].normInd;
+
+                    Vertex vertex = new Vertex();
+                    vertex.loc = locs[iLoc];
+                    vertex.uv = uvs[iuv];
+                    vertex.norm = norms[inorm];
+                    vertBufData.Add(vertex);
+                }
+
                 int nbytes = 16 + 32 * (int)nverts;
                 byte[] data = new byte[nbytes];
                 data[0] = (byte)nbytes;
@@ -247,6 +299,7 @@ namespace ModelProcessor
                 bw.Write(maxx);
                 bw.Write(maxy);
                 bw.Write(maxz);
+                bw.Write(maxr);
                 bw.Write(nverts);
 
                 for (int i=0; i < nverts; i++)
@@ -275,12 +328,13 @@ namespace ModelProcessor
             }
 
             vertBufData = new List<Vertex>();
-            maxx = maxy = maxz = 0;
+            maxx = maxy = maxz = maxr = 0;
             nverts = 0;
 
             textBox1.Text = maxx.ToString();
             textBox2.Text = maxy.ToString();
             textBox3.Text = maxz.ToString();
+            textBox5.Text = maxr.ToString();
             textBox4.Text = nverts.ToString();
 
             // enable the buttons
